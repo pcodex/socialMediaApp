218bec33a7c3335f702d0ac32252d9bc31e47bdf
Prab <prab.missier@gmail.com>
2020-03-12 17:58:41 +1100

    Deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI:'mongodb+srv://socialmediaapp:socialmediaapp1@onlinedatingapp-s07xw.mongodb.net/test?retryWrites=true&w=majority',
+    GoogleClientID : '',
+    GoogleClientSecret:''
+}

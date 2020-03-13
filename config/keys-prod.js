324d6be54ebd565b2d893cbb2cda4d10a7abddb4
Prab <prab.missier@gmail.com>
2020-03-13 15:10:00 +1100

    Setup config vars in heroku

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI:'mongodb+srv://socialmediaapp:socialmediaapp1@onlinedatingapp-s07xw.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID : '275410917617-drk54e7fjdu8fl40r975558np3ke263v.apps.googleusercontent.com',
-    GoogleClientSecret:'HB6_eZJr-X4rQejmpuq-Odye'
+    MongoURI:process.env.MONGO_URI,
+    GoogleClientID : process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret:process.env.GOOGLE_CLIENT_SECRET
 }
